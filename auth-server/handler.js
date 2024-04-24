a928ec258e3d08b407643f473e84100da98b0ab3
Userman1986 <e.samonin1986@gmail.com>
2024-04-24 08:30:25 +0200

    15

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,7 +5,7 @@ const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-  "https://userman1986.github.io/meet/"
+  "https://userman1986.github.io/Met/"
 ];
 
 const oAuth2Client = new google.auth.OAuth2(
